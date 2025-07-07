2fc0b1ec44d46f2ad1d1b9adb37b567732ef7683
PopJoker <52096871+PopJoker@users.noreply.github.com>
2025-07-07 23:33:48 +0800

    Update Form_ModbusRTU_Recive.cs

--- Form_ModbusRTU_Recive.cs
+++ Form_ModbusRTU_Recive.cs
@@ -30,10 +30,39 @@ namespace SerialReceiver
 
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            string data = serialPort.ReadExisting();
+            int bytesToRead = serialPort.BytesToRead;
+            byte[] buffer = new byte[bytesToRead];
+            serialPort.Read(buffer, 0, bytesToRead);
+
+            string hexString = BitConverter.ToString(buffer).Replace("-", " ");
+
+    // 假設 Modbus RTU 回應格式: SlaveID(1) + Func(1) + ByteCount(1) + Data(ByteCount) + CRC(2)
+            if (buffer.Length >= 5)
+            {
+                int byteCount = buffer[2];
+                if (buffer.Length >= 3 + byteCount + 2)
+                {
+                    byte[] dataBytes = new byte[byteCount];
+                    Array.Copy(buffer, 3, dataBytes, 0, byteCount);
+                    string asciiData = System.Text.Encoding.ASCII.GetString(dataBytes);
+
+                    Invoke(new Action(() =>
+                    {
+                        txtReceived.AppendText($"HEX: {hexString}\r\nASCII資料區: {asciiData}\r\n");
+                    }));
+                    return;//不想用else :D
+                }
+            }
+
+        // 若格式不符，至少顯示 HEX
+            Invoke(new Action(() =>
+            {
+                txtReceived.AppendText($"HEX: {hexString}\r\n");
+            }));
+            /*string data = serialPort.ReadExisting();
             Invoke(new Action(() => {
                 txtReceived.AppendText("收到: " + data + Environment.NewLine);
-            }));
+            }));*/
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
